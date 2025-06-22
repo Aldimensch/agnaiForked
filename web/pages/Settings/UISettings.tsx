3c98b2d92d79881445af0d49c586af8d830ef9b3
Aldimensch <aldimensch@gmail.com>
2025-06-22 10:43:09 +0200

    Who is formmating this so badly?

--- web/pages/Settings/UISettings.tsx
+++ web/pages/Settings/UISettings.tsx
@@ -363,7 +363,10 @@ const UISettings: Component<{}> = () => {
         label="Chat Quote Emphasis Color"
         fieldName="chatQuoteEmphasisColor"
         helperText={
-          <span class="link" onClick={() => userStore.saveCustomUI({ chatQuoteEmphasisColor: 'text-800' })}>
+          <span
+            class="link"
+            onClick={() => userStore.saveCustomUI({ chatQuoteEmphasisColor: 'text-800' })}
+          >
             Reset to Default
           </span>
         }
@@ -371,7 +374,7 @@ const UISettings: Component<{}> = () => {
         onChange={(color) => userStore.saveCustomUI({ chatQuoteEmphasisColor: color })}
         value={state.current.chatQuoteEmphasisColor || '--text-800'}
       />
-      
+
       <Select
         fieldName="chatQuoteEmphasisWeight"
         label="Chat quote emphasis bold"
@@ -380,8 +383,9 @@ const UISettings: Component<{}> = () => {
           { label: 'None', value: 'unset' },
           { label: 'Bold', value: 'bold' },
         ]}
-        
-        onChange={(item) => userStore.saveCustomUI({ chatQuoteEmphasisWeight: item.value as string })}
+        onChange={(item) =>
+          userStore.saveCustomUI({ chatQuoteEmphasisWeight: item.value as string })
+        }
         value={state.current.chatQuoteEmphasisWeight || 'unset'}
       />
 
